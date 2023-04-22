5c04e0cbd9a4c8001656a9c9033a8da148edcd02
Kenneth <nitramnek@gmail.com>
2023-04-22 10:53:24 +0300

    Assignment Done

---
+++ index.js
@@ -0,0 +1,8 @@
+//k3n5c0d3
+// const divide= ()=> 2000/100
+function divide (){
+    divide =(2000/100)
+    return(divide)
+} 
+const square=(number)=>number*number
+const add=(num1, num2)=>num1+num2
